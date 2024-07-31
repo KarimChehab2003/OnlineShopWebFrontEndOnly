502bd83ff8d2a514b20752921e3b8e89c2c0b1e8
KarimChehab2003 <karimchehab2018@gmail.com>
2024-07-31 22:34:47 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// var productContentDiv = document.createElement('div')
+// var productImageDiv = document.createElement('div');
+// var productImage = document.createElement('img');
+// var productInfo = document.createElement('div');
+// var productCategory = document.createElement('p')
+// var productName = document.createElement('p')
+// var starRatingDiv = document.createElement('div')
+// var starIcon = document.createElement('i')
+// var productPriceDiv = document.createElement('div')
+// var originalPrice = document.createElement('p')
+// var salePrice = document.createElement('p')
+
+// productImageDiv.appendChild(productImage);
+
+// productInfo.appendChild(productCategory)
+// productInfo.appendChild(productName)
+// productInfo.appendChild(starRatingDiv)
+
+// productPriceDiv.appendChild(originalPrice)
+// productPriceDiv.appendChild(salePrice)
+
+// productContentDiv.appendChild(productImageDiv)
+// productContentDiv.appendChild(productInfo)
+// productContentDiv.appendChild(productPriceDiv)
